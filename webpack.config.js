cfd206a618c3bfc5a9ac6c3b9b15c884b7ab7f2d
havefive <lzc09008@gmail.com>
2017-08-24 22:17:07 +0800

    init config

---
+++ webpack.config.js
@@ -0,0 +1,98 @@
+const path = require('path');
+const fs = require('fs');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+function getEntry() {
+    var jsPath = path.resolve('./public/assets/js', '');
+    var dirs = fs.readdirSync(jsPath);
+    var matchs = [], files = {};
+    dirs.forEach(function (item) {
+        matchs = item.match(/(.+)\.js$/);
+        if (matchs) {
+            files[matchs[1]] = path.resolve('./public/assets/js', '', item);
+        }
+    });
+    // console.log(files);
+    return files;
+}
+
+module.exports = {
+    context: path.resolve(__dirname, './public'),
+    node: {
+        fs: 'empty'
+    },
+    entry: getEntry(),
+    output: {
+        path: path.resolve(__dirname, './dist/static'),
+        filename: 'js/[name].min.js',
+        publicPath: '/static'
+    },
+    module: {
+        rules: [
+        {
+            test: /\.css$/,
+            use: 'css-loader',
+            use: ExtractTextPlugin.extract({
+                use: ['css-loader']
+            })
+
+        },
+        {
+            test: /\.less$/,
+            use: [
+            'style-loader',
+            'less-loader'
+            ],
+            use: ExtractTextPlugin.extract({
+              fallback: 'style-loader',
+              //resolve-url-loader may be chained before sass-loader if necessary 
+              use: ['css-loader', 'less-loader']
+          })
+        },
+        {
+            test: /\.vue$/,
+            loader: 'vue-loader'
+
+        },
+        {
+            test: /\.js$/,
+            loader: 'babel-loader',
+            /* 排除模块安装目录的文件 */
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015','react']
+            }
+        },
+        {
+            test: /\.scss$/,
+            use: [
+            'style-loader',
+            'sass-loader'
+            ],
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+              //resolve-url-loader may be chained before sass-loader if necessary 
+              use: ['css-loader', 'sass-loader']
+          })
+        }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin({filename:'css/app.css'}),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new webpack.optimize.ModuleConcatenationPlugin(),
+        new CopyWebpackPlugin([
+            // {output}/to/file.txt
+            { from: './**.html', to: '../' }]
+            ),
+    ],
+    devServer: {
+        contentBase: path.resolve(__dirname, './public'),  // New
+    }
+};
